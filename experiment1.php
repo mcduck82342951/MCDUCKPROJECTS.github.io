4b9d273f4d07d11cc3143263782c63c46f120dc0
mcduck82342951 <85113714+mcduck82342951@users.noreply.github.com>
2021-06-05 14:51:50 +0300

    Add files via upload

---
+++ experiment1.php
@@ -0,0 +1,8 @@
+<?php
+echo "Privetstvuyu vas zdes. Slishal, vi uchites na Php-programmista!\n";
+echo "Pochemu bi vam ne nabrat svoyo imya dlya mena:\n"
+
+$name=trim(fgets(STDIN));
+
+echo "\nThank you, " .$name. "ochen rad s vami poznakomitsa";
+?>
