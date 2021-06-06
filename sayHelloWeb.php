bd76de9ba813daab019ab780a2a5679b55e38be1
Mirzobakhtiyor Rahmatov <85113714+mcduck82342951@users.noreply.github.com>
2021-06-06 03:27:25 +0300

    Add files via upload

---
+++ sayHelloWeb.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  	<link rel="stylesheet"  href="phpMM.css" type="text/css">
+  </head>
+<body>
+	<div id="header"><h1>PHP &  MySQL: The Missing Manual</h1></div>
+	<div id="example">Example 1.1</div>
+
+	<div id="content">
+		<h1>Hello, <?php echo $_REQUEST['name'] ?>!</h1>
+		<p>We are welcome you. Welcome to the start of out odyssey in the world of PHP.</p>
+	</form>
+</div>
